3354d1d0f11707ab7f0eb3d18b37c5b672edc4ba
mohuishou <1@lailin.xyz>
2017-02-06 15:37:53 +0800

    update

--- Bubble/Bubble.go
+++ Bubble/Bubble.go
@@ -1,7 +1,24 @@
-package Bubble //冒泡排序
+//Package bubble 冒泡排序
+package bubble
 
 // SortInt int数组排序
+// a[]int
 func SortInt(a []int) []int {
+	aLen := len(a)
+	for i := 0; i < aLen-1; i++ {
+		for j := 0; j < aLen-i-1; j++ {
+			if a[j] > a[j+1] {
+				tmp := a[j+1]
+				a[j+1] = a[j]
+				a[j] = tmp
+			}
+		}
+	}
+	return a
+}
+
+//SortInt32 int32数组排序
+func SortInt32(a []int) []int {
 	aLen := len(a)
 	for i := 0; i < aLen-1; i++ {
 		for j := 0; j < aLen-i-1; j++ {
--- Bubble/example/main.go
+++ Bubble/example/main.go
@@ -10,16 +10,6 @@ func main() {
 	//冒泡排序
 	a := []int{8, 6, 1, 4, 3, 9, 5, 65, 66, 0}
 	fmt.Println("原始数组：", a)
-	// aLen := len(a)
-	// for i := 0; i < aLen-1; i++ {
-	// 	for j := 0; j < aLen-i-1; j++ {
-	// 		if a[j] > a[j+1] {
-	// 			tmp := a[j+1]
-	// 			a[j+1] = a[j]
-	// 			a[j] = tmp
-	// 		}
-	// 	}
-	// }
-	b := Bubble.SortInt(a)
+	b := bubble.SortInt(a)
 	fmt.Println("排序数组：", b)
 }
