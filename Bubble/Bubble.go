07c674e82e7fd42902ac0c172f920a72be715caa
mohuishou <1@lailin.xyz>
2017-02-06 16:09:56 +0800

    优化：冒泡排序

--- Bubble/Bubble.go
+++ Bubble/Bubble.go
@@ -2,32 +2,31 @@
 package bubble
 
 // SortInt int数组排序
-// a[]int
 func SortInt(a []int) []int {
-	aLen := len(a)
-	for i := 0; i < aLen-1; i++ {
-		for j := 0; j < aLen-i-1; j++ {
-			if a[j] > a[j+1] {
-				tmp := a[j+1]
-				a[j+1] = a[j]
-				a[j] = tmp
-			}
-		}
-	}
-	return a
-}
 
-//SortInt32 int32数组排序
-func SortInt32(a []int) []int {
 	aLen := len(a)
-	for i := 0; i < aLen-1; i++ {
-		for j := 0; j < aLen-i-1; j++ {
+
+	for i := aLen - 1; i >= 0; i-- {
+		//标记，如果flag一次冒泡之后没有改变，那么证明排序已完成，不需要再次排序，直接退出
+		flag := 0
+
+		//一次冒泡
+		for j := 0; j < i; j++ {
 			if a[j] > a[j+1] {
-				tmp := a[j+1]
-				a[j+1] = a[j]
-				a[j] = tmp
+				//交换两个变量的值，无需引入临时变量
+				a[j], a[j+1] = a[j+1], a[j]
+
+				//有交换，flag=1
+				flag = 1
 			}
 		}
+
+		//判断一次冒泡，是否存在交换
+		if flag == 0 {
+			break
+		}
+
 	}
+
 	return a
 }
