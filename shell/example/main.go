48e34206365619b83feb3c23fd472c33d2515d1b
mohuishou <1@lailin.xyz>
2017-02-07 09:53:24 +0800

    新增hibbard算法

--- shell/example/main.go
+++ shell/example/main.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
-	a := []int{34, 8, 64, 51, 32, 21}
+	a := []int{34, 8, 64, 51, 32, 21, 5, 3, 6, 9, 8, 7, 4, 5, 2, 3, 4}
 	fmt.Println("原始数组：", a)
-	b, n := shell.SortInt(a)
+	b, n := shell.SortHibbardInt(a)
 	fmt.Println("排序数组：", b)
 	fmt.Println("交换次数：", n)
 }
--- shell/shell.go
+++ shell/shell.go
@@ -1,7 +1,8 @@
 //Package shell 排序
 package shell
 
-//SortInt 传统shell排序
+//SortInt 传统shell排序,间隔为N/2
+//相邻间隔可能不互质，可能会出现前置排序无用的情况
 func SortInt(a []int) ([]int, int) {
 	n := 0
 	aLen := len(a)
@@ -9,6 +10,29 @@ func SortInt(a []int) ([]int, int) {
 	//定义间隔
 	for i := aLen / 2; i > 0; i = i / 2 {
 
+		//插入排序
+		for j := i; j < aLen; j++ {
+			tmp := a[j]
+			k := j
+			for ; k >= i && tmp < a[k-i]; k = k - i {
+				a[k] = a[k-i]
+				n++
+			}
+			a[k] = tmp
+		}
+	}
+	return a, n
+}
+
+//SortHibbardInt Hibbard算法，间隔为2^k-1
+func SortHibbardInt(a []int) ([]int, int) {
+	n, i := 0, 0
+	aLen := len(a)
+	for i = 1; i <= aLen-1; i = i*2 + 1 {
+	}
+	//定义间隔
+	for ; i > 0; i = (i - 1) / 2 {
+		// println(i)
 		//插入排序
 		for j := i; j < aLen; j++ {
 			tmp := a[j]
